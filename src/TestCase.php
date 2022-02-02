4dddb28b59703dd5ffe05e5e78e659f5a0a23679
Ryan Thompson <ryan@pyrocms.com>
2022-02-02 13:08:52 -0600

    Restore streams data upon teardown

--- src/TestCase.php
+++ src/TestCase.php
@@ -2,10 +2,34 @@
 
 namespace Streams\Testing;
 
+use Illuminate\Support\Facades\File;
+
 class TestCase extends \Orchestra\Testbench\TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->restoreStreamsData();
+
+        parent::tearDown();
+    }
+
     public static function applicationBasePath()
     {
         return dirname(__DIR__) . '/laravel';
+    }
+
+    protected function restoreStreamsData()
+    {
+        File::deleteDirectory(__DIR__ . '/../laravel/streams');
+
+        File::copyDirectory(
+            __DIR__ . '/../laravel/streams.bak',
+            __DIR__ . '/../laravel/streams'
+        );
     }
 }
