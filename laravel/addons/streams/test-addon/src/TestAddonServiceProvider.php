86875c40b802ad233c771f95f9d79808611ca616
Ryan Thompson <ryan@pyrocms.com>
2022-02-05 00:11:25 -0600

    Test addon

---
+++ laravel/addons/streams/test-addon/src/TestAddonServiceProvider.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace Streams\TestAddon;
+
+use Illuminate\Support\ServiceProvider;
+
+class TestAddonServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        # code...
+    }
+
+    public function boot()
+    {
+        # code...
+    }
+}
