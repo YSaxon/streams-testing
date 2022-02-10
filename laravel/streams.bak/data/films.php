f4039f6ec73091a70e19bc2578444adf61fb98ce
Ryan Thompson <ryan@pyrocms.com>
2022-02-09 23:51:40 -0600

    PHP file data

--- laravel/streams.bak/data/films.php
+++ laravel/streams.bak/data/films.php
@@ -1,6 +1,598 @@
 <?php
 
 return [
+    4 => [
+        'episode_id' => 4,
+        'title' => 'A New Hope',
+        'opening_crawl' => 'It is a period of civil war.
+Rebel spaceships, striking
+from a hidden base, have won
+their first victory against
+the evil Galactic Empire.
+During the battle, Rebel
+spies managed to steal secret
+plans to the Empire\'s
+ultimate weapon, the DEATH
+STAR, an armored space
+station with enough power
+to destroy an entire planet.
+Pursued by the Empire\'s
+sinister agents, Princess
+Leia races home aboard her
+starship, custodian of the
+stolen plans that can save her
+people and restore
+freedom to the galaxy....',
+        'director' => 'George Lucas',
+        'producer' => 'Gary Kurtz, Rick McCallum',
+        'release_date' => '1977-05-25',
+        'characters' => [
+            0 => 1,
+            1 => 2,
+            2 => 3,
+            3 => 4,
+            4 => 5,
+            5 => 6,
+            6 => 7,
+            7 => 8,
+            8 => 9,
+            9 => 10,
+            10 => 12,
+            11 => 13,
+            12 => 14,
+            13 => 15,
+            14 => 16,
+            15 => 18,
+            16 => 19,
+            17 => 81,
+        ],
+        'planets' => [
+            0 => 1,
+            1 => 2,
+            2 => 3,
+        ],
+        'starships' => [
+            0 => 2,
+            1 => 3,
+            2 => 5,
+            3 => 9,
+            4 => 10,
+            5 => 11,
+            6 => 12,
+            7 => 13,
+        ],
+        'vehicles' => [
+            0 => 4,
+            1 => 6,
+            2 => 7,
+            3 => 8,
+        ],
+        'species' => [
+            0 => 1,
+            1 => 2,
+            2 => 3,
+            3 => 4,
+            4 => 5,
+        ],
+        'created' => '2014-12-10 14:23:31',
+        'edited' => '2014-12-20 19:49:45',
+    ],
+    5 => [
+        'episode_id' => 5,
+        'title' => 'The Empire Strikes Back',
+        'opening_crawl' => 'It is a dark time for the
+Rebellion. Although the Death
+Star has been destroyed,
+Imperial troops have driven the
+Rebel forces from their hidden
+base and pursued them across
+the galaxy.
+Evading the dreaded Imperial
+Starfleet, a group of freedom
+fighters led by Luke Skywalker
+has established a new secret
+base on the remote ice world
+of Hoth.
+The evil lord Darth Vader,
+obsessed with finding young
+Skywalker, has dispatched
+thousands of remote probes into
+the far reaches of space....',
+        'director' => 'Irvin Kershner',
+        'producer' => 'Gary Kurtz, Rick McCallum',
+        'release_date' => '1980-05-17',
+        'characters' => [
+            0 => 1,
+            1 => 2,
+            2 => 3,
+            3 => 4,
+            4 => 5,
+            5 => 10,
+            6 => 13,
+            7 => 14,
+            8 => 18,
+            9 => 20,
+            10 => 21,
+            11 => 22,
+            12 => 23,
+            13 => 24,
+            14 => 25,
+            15 => 26,
+        ],
+        'planets' => [
+            0 => 4,
+            1 => 5,
+            2 => 6,
+            3 => 27,
+        ],
+        'starships' => [
+            0 => 3,
+            1 => 10,
+            2 => 11,
+            3 => 12,
+            4 => 15,
+            5 => 17,
+            6 => 21,
+            7 => 22,
+            8 => 23,
+        ],
+        'vehicles' => [
+            0 => 8,
+            1 => 14,
+            2 => 16,
+            3 => 18,
+            4 => 19,
+            5 => 20,
+        ],
+        'species' => [
+            0 => 1,
+            1 => 2,
+            2 => 3,
+            3 => 6,
+            4 => 7,
+        ],
+        'created' => '2014-12-12 11:26:24',
+        'edited' => '2014-12-15 13:07:53',
+    ],
+    6 => [
+        'episode_id' => 6,
+        'title' => 'Return of the Jedi',
+        'opening_crawl' => 'Luke Skywalker has returned to
+his home planet of Tatooine in
+an attempt to rescue his
+friend Han Solo from the
+clutches of the vile gangster
+Jabba the Hutt.
+Little does Luke know that the
+GALACTIC EMPIRE has secretly
+begun construction on a new
+armored space station even
+more powerful than the first
+dreaded Death Star.
+When completed, this ultimate
+weapon will spell certain doom
+for the small band of rebels
+struggling to restore freedom
+to the galaxy...',
+        'director' => 'Richard Marquand',
+        'producer' => 'Howard G. Kazanjian, George Lucas, Rick McCallum',
+        'release_date' => '1983-05-25',
+        'characters' => [
+            0 => 1,
+            1 => 2,
+            2 => 3,
+            3 => 4,
+            4 => 5,
+            5 => 10,
+            6 => 13,
+            7 => 14,
+            8 => 16,
+            9 => 18,
+            10 => 20,
+            11 => 21,
+            12 => 22,
+            13 => 25,
+            14 => 27,
+            15 => 28,
+            16 => 29,
+            17 => 30,
+            18 => 31,
+            19 => 45,
+        ],
+        'planets' => [
+            0 => 1,
+            1 => 5,
+            2 => 7,
+            3 => 8,
+            4 => 9,
+        ],
+        'starships' => [
+            0 => 2,
+            1 => 3,
+            2 => 10,
+            3 => 11,
+            4 => 12,
+            5 => 15,
+            6 => 17,
+            7 => 22,
+            8 => 23,
+            9 => 27,
+            10 => 28,
+            11 => 29,
+        ],
+        'vehicles' => [
+            0 => 8,
+            1 => 16,
+            2 => 18,
+            3 => 19,
+            4 => 24,
+            5 => 25,
+            6 => 26,
+            7 => 30,
+        ],
+        'species' => [
+            0 => 1,
+            1 => 2,
+            2 => 3,
+            3 => 5,
+            4 => 6,
+            5 => 8,
+            6 => 9,
+            7 => 10,
+            8 => 15,
+        ],
+        'created' => '2014-12-18 10:39:33',
+        'edited' => '2014-12-20 09:48:37',
+    ],
+    1 => [
+        'episode_id' => 1,
+        'title' => 'The Phantom Menace',
+        'opening_crawl' => 'Turmoil has engulfed the
+Galactic Republic. The taxation
+of trade routes to outlying star
+systems is in dispute.
+Hoping to resolve the matter
+with a blockade of deadly
+battleships, the greedy Trade
+Federation has stopped all
+shipping to the small planet
+of Naboo.
+While the Congress of the
+Republic endlessly debates
+this alarming chain of events,
+the Supreme Chancellor has
+secretly dispatched two Jedi
+Knights, the guardians of
+peace and justice in the
+galaxy, to settle the conflict....',
+        'director' => 'George Lucas',
+        'producer' => 'Rick McCallum',
+        'release_date' => '1999-05-19',
+        'characters' => [
+            0 => 2,
+            1 => 3,
+            2 => 10,
+            3 => 11,
+            4 => 16,
+            5 => 20,
+            6 => 21,
+            7 => 32,
+            8 => 33,
+            9 => 34,
+            10 => 35,
+            11 => 36,
+            12 => 37,
+            13 => 38,
+            14 => 39,
+            15 => 40,
+            16 => 41,
+            17 => 42,
+            18 => 43,
+            19 => 44,
+            20 => 46,
+            21 => 47,
+            22 => 48,
+            23 => 49,
+            24 => 50,
+            25 => 51,
+            26 => 52,
+            27 => 53,
+            28 => 54,
+            29 => 55,
+            30 => 56,
+            31 => 57,
+            32 => 58,
+            33 => 59,
+        ],
+        'planets' => [
+            0 => 1,
+            1 => 8,
+            2 => 9,
+        ],
+        'starships' => [
+            0 => 31,
+            1 => 32,
+            2 => 39,
+            3 => 40,
+            4 => 41,
+        ],
+        'vehicles' => [
+            0 => 33,
+            1 => 34,
+            2 => 35,
+            3 => 36,
+            4 => 37,
+            5 => 38,
+            6 => 42,
+        ],
+        'species' => [
+            0 => 1,
+            1 => 2,
+            2 => 6,
+            3 => 11,
+            4 => 12,
+            5 => 13,
+            6 => 14,
+            7 => 15,
+            8 => 16,
+            9 => 17,
+            10 => 18,
+            11 => 19,
+            12 => 20,
+            13 => 21,
+            14 => 22,
+            15 => 23,
+            16 => 24,
+            17 => 25,
+            18 => 26,
+            19 => 27,
+        ],
+        'created' => '2014-12-19 16:52:55',
+        'edited' => '2014-12-20 10:54:07',
+    ],
+    2 => [
+        'episode_id' => 2,
+        'title' => 'Attack of the Clones',
+        'opening_crawl' => 'There is unrest in the Galactic
+Senate. Several thousand solar
+systems have declared their
+intentions to leave the Republic.
+This separatist movement,
+under the leadership of the
+mysterious Count Dooku, has
+made it difficult for the limited
+number of Jedi Knights to maintain 
+peace and order in the galaxy.
+Senator Amidala, the former
+Queen of Naboo, is returning
+to the Galactic Senate to vote
+on the critical issue of creating
+an ARMY OF THE REPUBLIC
+to assist the overwhelmed
+Jedi....',
+        'director' => 'George Lucas',
+        'producer' => 'Rick McCallum',
+        'release_date' => '2002-05-16',
+        'characters' => [
+            0 => 2,
+            1 => 3,
+            2 => 6,
+            3 => 7,
+            4 => 10,
+            5 => 11,
+            6 => 20,
+            7 => 21,
+            8 => 22,
+            9 => 33,
+            10 => 35,
+            11 => 36,
+            12 => 40,
+            13 => 43,
+            14 => 46,
+            15 => 51,
+            16 => 52,
+            17 => 53,
+            18 => 58,
+            19 => 59,
+            20 => 60,
+            21 => 61,
+            22 => 62,
+            23 => 63,
+            24 => 64,
+            25 => 65,
+            26 => 66,
+            27 => 67,
+            28 => 68,
+            29 => 69,
+            30 => 70,
+            31 => 71,
+            32 => 72,
+            33 => 73,
+            34 => 74,
+            35 => 75,
+            36 => 76,
+            37 => 77,
+            38 => 78,
+            39 => 82,
+        ],
+        'planets' => [
+            0 => 1,
+            1 => 8,
+            2 => 9,
+            3 => 10,
+            4 => 11,
+        ],
+        'starships' => [
+            0 => 21,
+            1 => 32,
+            2 => 39,
+            3 => 43,
+            4 => 47,
+            5 => 48,
+            6 => 49,
+            7 => 52,
+            8 => 58,
+        ],
+        'vehicles' => [
+            0 => 4,
+            1 => 44,
+            2 => 45,
+            3 => 46,
+            4 => 50,
+            5 => 51,
+            6 => 53,
+            7 => 54,
+            8 => 55,
+            9 => 56,
+            10 => 57,
+        ],
+        'species' => [
+            0 => 1,
+            1 => 2,
+            2 => 6,
+            3 => 12,
+            4 => 13,
+            5 => 15,
+            6 => 28,
+            7 => 29,
+            8 => 30,
+            9 => 31,
+            10 => 32,
+            11 => 33,
+            12 => 34,
+            13 => 35,
+        ],
+        'created' => '2014-12-20 10:57:57',
+        'edited' => '2014-12-20 20:18:48',
+    ],
+    3 => [
+        'episode_id' => 3,
+        'title' => 'Revenge of the Sith',
+        'opening_crawl' => 'War! The Republic is crumbling
+under attacks by the ruthless
+Sith Lord, Count Dooku.
+There are heroes on both sides.
+Evil is everywhere.
+In a stunning move, the
+fiendish droid leader, General
+Grievous, has swept into the
+Republic capital and kidnapped
+Chancellor Palpatine, leader of
+the Galactic Senate.
+As the Separatist Droid Army
+attempts to flee the besieged
+capital with their valuable
+hostage, two Jedi Knights lead a
+desperate mission to rescue the
+captive Chancellor....',
+        'director' => 'George Lucas',
+        'producer' => 'Rick McCallum',
+        'release_date' => '2005-05-19',
+        'characters' => [
+            0 => 1,
+            1 => 2,
+            2 => 3,
+            3 => 4,
+            4 => 5,
+            5 => 6,
+            6 => 7,
+            7 => 10,
+            8 => 11,
+            9 => 12,
+            10 => 13,
+            11 => 20,
+            12 => 21,
+            13 => 33,
+            14 => 35,
+            15 => 46,
+            16 => 51,
+            17 => 52,
+            18 => 53,
+            19 => 54,
+            20 => 55,
+            21 => 56,
+            22 => 58,
+            23 => 63,
+            24 => 64,
+            25 => 67,
+            26 => 68,
+            27 => 75,
+            28 => 78,
+            29 => 79,
+            30 => 80,
+            31 => 81,
+            32 => 82,
+            33 => 83,
+        ],
+        'planets' => [
+            0 => 1,
+            1 => 2,
+            2 => 5,
+            3 => 8,
+            4 => 9,
+            5 => 12,
+            6 => 13,
+            7 => 14,
+            8 => 15,
+            9 => 16,
+            10 => 17,
+            11 => 18,
+            12 => 19,
+        ],
+        'starships' => [
+            0 => 2,
+            1 => 32,
+            2 => 48,
+            3 => 59,
+            4 => 61,
+            5 => 63,
+            6 => 64,
+            7 => 65,
+            8 => 66,
+            9 => 68,
+            10 => 74,
+            11 => 75,
+        ],
+        'vehicles' => [
+            0 => 33,
+            1 => 50,
+            2 => 53,
+            3 => 56,
+            4 => 60,
+            5 => 62,
+            6 => 67,
+            7 => 69,
+            8 => 70,
+            9 => 71,
+            10 => 72,
+            11 => 73,
+            12 => 76,
+        ],
+        'species' => [
+            0 => 1,
+            1 => 2,
+            2 => 3,
+            3 => 6,
+            4 => 15,
+            5 => 19,
+            6 => 20,
+            7 => 23,
+            8 => 24,
+            9 => 25,
+            10 => 26,
+            11 => 27,
+            12 => 28,
+            13 => 29,
+            14 => 30,
+            15 => 33,
+            16 => 34,
+            17 => 35,
+            18 => 36,
+            19 => 37,
+        ],
+        'created' => '2014-12-20 18:49:38',
+        'edited' => '2014-12-20 20:47:52',
+    ],
     7 => [
         'episode_id' => 7,
         'title' => 'The Force Awakens',
